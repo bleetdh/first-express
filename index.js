b40db6d90fc9e3cc27689c038a2fd2a212f4e841
lee brian <leesinshingbrian@gmail.com>
2017-07-17 11:31:32 +0800

    handlebars

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
 const express = require('express')
 const app = express() // open express server
+const exphbs = require('express-handlebars') // handlebars logic
 
 // all request will have to go thru this middleware
-app.use(function (req, res, next) {
-  console.log('first middleware')
-  next()
-})
-
 // all request will have to know that the public folder is where we put our static files
 app.use(express.static('public'))
 
+// setting dynamic view folders
+app.engine('handlebars', exphbs(
+  // handlebars config
+  {
+    defaultLayout: 'main',
+    partialsDir: 'views/partials'
+  }
+))
+app.set('view engine', 'handlebars')
+
 // listening for request
 app.get('/', function (req, res) {
-  res.sendFile('index.html')
+  // this will be your search thru db
+  var user = {
+    name: 'brian',
+    email: 'blee@email.com',
+    isAdmin: true
+  }
+  var blogs = [
+    {
+      title: 'my blog post',
+      content: 'here\'s my blogpost'
+    },
+    {
+      title: 'my second blog post',
+      content: 'here\'s my second blogpost'
+    }
+  ]
+  res.render('index', {
+    user: user,
+    blogs: blogs
+  })
 })
 
-app.get('/about/:name', function (req, res) {
-  res.send(req.params)
+app.get('/about', function (req, res) {
+  res.render('about')
 })
 
 app.get('/faq', function (req, res) {
-  res.send('faq page')
+  res.render('faq')
 })
 
 app.post('/')
